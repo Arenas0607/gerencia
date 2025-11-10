59fd5e67500b24ff586757c6bf6586590c774b2b
Arenas0607 <cristian100128@gmail.com>
2025-11-09 23:37:59 -0500

    notificacionMEJORAFINAL

--- app.py
+++ app.py
@@ -1,21 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import requests
 
 app = Flask(__name__)
 
-DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1437282453994602496/PLfb3QMAEHmIt2x-oF7y3Lr10W4SHeeYCjQ4W-8Fgm8pv72Rg8iyVD1C-lZ0yEwbf5r7"
+
+DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1437289372054720594/2N-AMKnjk2bvP01TV0dD-d5H0OaLXxIWkTQk_mfDvT0KA5gxgHpVnSOWwVTYOgiRuWdX"
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/alerta', methods=['POST'])
-def alerta():
-    message = {
-        "content": "⚠️ El usuario no ha interactuado. ¿Deseas reportar alguna novedad?"
+@app.route('/enviar_discord', methods=['POST'])
+def enviar_discord():
+    data = request.json
+    lat = data.get("lat")
+    lon = data.get("lon")
+
+    mensaje = {
+        "embeds": [
+            {
+                "title": "🆘 Reporte de usuario !varado",
+                "description": f"📍 [Ver ubicación en Google Maps](https://www.google.com/maps?q={lat},{lon})",
+                "color": 3447003  # Azul
+            }
+        ]
     }
-    requests.post(DISCORD_WEBHOOK, json=message)
-    return "Mensaje enviado a Discord", 200
+    response = requests.post(DISCORD_WEBHOOK, json=mensaje)
+    return jsonify({"status": "ok", "discord_response": response.status_code})
 
 if __name__ == '__main__':
     app.run(debug=True)
